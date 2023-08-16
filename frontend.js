44a60e93f5c60036ebec905cef1a45e75607c194
swapnil chavan <chavanswapnil822@gmail.com>
2023-08-16 18:26:36 +0530

    updated

--- frontend.js
+++ frontend.js
@@ -10,11 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const imageFile = imageInput.files[0];
+        
+        // Convert the image to JPEG format
+        const convertedImage = await convertToJPEG(imageFile);
+
         const formData = new FormData();
-        formData.append("image", imageFile);
+        formData.append("image", convertedImage, "converted_image.jpg");
 
         try {
-            const response = await fetch("/upload", {
+            const response = await fetch("https://swapnil-mock-server.onrender.com/upload", {
                 method: "POST",
                 body: formData,
             });
@@ -30,5 +34,35 @@ document.addEventListener("DOMContentLoaded", () => {
     function showMessage(message, type) {
         messageDiv.textContent = message;
         messageDiv.className = type;
+    }
+
+    async function convertToJPEG(imageFile) {
+        const image = new Image();
+        const reader = new FileReader();
+
+        reader.readAsDataURL(imageFile);
+
+        return new Promise((resolve, reject) => {
+            reader.onload = (event) => {
+                image.src = event.target.result;
+
+                image.onload = () => {
+                    const canvas = document.createElement("canvas");
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+
+                    const ctx = canvas.getContext("2d");
+                    ctx.drawImage(image, 0, 0);
+
+                    canvas.toBlob((blob) => {
+                        resolve(blob);
+                    }, "image/jpeg", 0.8);
+                };
+
+                image.onerror = (error) => {
+                    reject(error);
+                };
+            };
+        });
     }
 });
